9c55da6b7dd277cc44900a8ed6283bb4049509fe
Kholik Nesonov <91988209+Nesaq@users.noreply.github.com>
2022-10-15 23:30:56 +0300

    init commander

--- bin/index.js
+++ bin/index.js
@@ -1,1 +1,15 @@
 #!/usr/bin/env node
+import { Command } from 'commander';
+
+const program = new Command();
+
+const outputDir = process.cwd();
+
+program.version('0.0.1', '-v, --vers', 'output the version number')
+  .option('-o, --output [dir]', 'output dir', `${outputDir}`)
+  .arguments('<url>')
+  .action((link, path) => {
+    // console.log(genDiff(link, path));
+    console.log(link, path);
+  });
+program.parse(program.argv);
